5c775407c2bd4b4c0a00e03abd3112da5f393451
Juan Acosta <116093967+juanillo02@users.noreply.github.com>
2023-10-31 11:03:32 +0100

    Rename cronometro.h to P2/cronometro.h

--- P2/cronometro.h
+++ P2/cronometro.h
@@ -16,18 +16,18 @@ public:
   void parar() { if (activo) { t1 = clock(); activo = false; } }
   double tiempo() const { return ((activo ? clock() : t1) - t0) / pps; }
 private:
-  bool activo;	   // Estado de actividad del cronómetro.
+  bool activo;	   // Estado de actividad del cronÃ³metro.
   clock_t t0, t1;  // Tiempos inicial y final.
 };
 
 // Notas:
 //
-// La función clock() devuelve el tiempo en unas unidades llamadas
-// «pulsos». Para convertir el tiempo a segundos es necesario dividir
-// por el número de pulsos por segundo. Esta cantidad aparece definida
+// La funciÃ³n clock() devuelve el tiempo en unas unidades llamadas
+// Â«pulsosÂ». Para convertir el tiempo a segundos es necesario dividir
+// por el nÃºmero de pulsos por segundo. Esta cantidad aparece definida
 // en la macro CLOCKS_PER_SEC cuyo tipo puede ser entero o de coma
 // flotante y que mantenemos en pps, que es de tipo double. Si
-// queremos hacer explícita una posible conversión de CLOCKS_PER_SEC a
+// queremos hacer explÃ­cita una posible conversiÃ³n de CLOCKS_PER_SEC a
 // double podemos modelar el tipo de tres formas:
 //
 // 1. Como en C: (double)CLOCKS_PER_SEC.
